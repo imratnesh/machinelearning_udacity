ba6d7b2f0a647c09f5eb66387b987d88f5a87cfa
Ratnesh Kushwaha <ratnesh.kush@yahoo.in>
2017-11-17 19:21:31 +0530

    Add files via upload

---
+++ final_project/build_email_features.py
@@ -0,0 +1,12 @@
+#import pickle
+import sys
+#import re
+#import os
+
+sys.path.append( "../tools/" )
+from parse_out_email_text import parseOutText
+
+from poi_email_addresses import poiEmails 
+
+#print len(poi_emails), len(fr[2:])
+
